9b3d1baa1629c1ecc00ccb03f800b06a7b81c2e0
¨Goodness <gtkbrain@gmail.com>
2017-05-28 22:35:43 +0100

    Initial commit to repo

---
+++ src/LaravelMoneywaveServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace GoodnessKay\LaravelMoneywave;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelMoneywaveServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
